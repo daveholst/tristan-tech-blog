a5f65792a7af3108b3e00c9cb0f68fe36775d9af
tristan <mousy93@hotmail.com>
2021-07-25 20:23:03 +0800

    index.html updated; style.css done for now;

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const dotenv = require("dotenv").config();
+const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
