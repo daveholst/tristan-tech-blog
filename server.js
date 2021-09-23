36349484a6477a7a53899cb7f1f7334eec0aa365
tristan <mousy93@hotmail.com>
2021-09-23 15:51:12 +0800

    schema added

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const sess = {
-  secret: "something secret",
+  secret: SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
