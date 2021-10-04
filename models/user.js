962b8cd4dc67c77f7c5d72c4a8890bb6c71cd4b8
tristan <mousy93@hotmail.com>
2021-10-04 15:24:11 +0800

    user model done for now; user seed created and done

--- models/user.js
+++ models/user.js
@@ -8,23 +8,40 @@ class User extends Model {
   }
 }
 
-User.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  userName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [6],
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6],
+      },
     },
   },
-});
+  {
+    hooks: {
+      beforeCreated: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 12);
+        return newUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: "user",
+    modelName: "user",
+  }
+);
+
+module.exports = User;
