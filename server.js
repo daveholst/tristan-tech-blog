a195993982c0ccc9e3772de0638c49c9aa3e0be1
tristan <mousy93@hotmail.com>
2021-10-05 13:29:56 +0800

    helper added for dates; server updated with require helpers

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sequelize = require("./config/connection");
 const routes = require("./controllers");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const sess = {
-  secret: SECRET,
+  secret: process.env.SECRET,
   cookie: {
     maxAge: 200000,
   },
