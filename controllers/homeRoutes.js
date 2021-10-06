f352ed35f7040e53564fbec739259f18594904f5
tristan <mousy93@hotmail.com>
2021-10-06 16:25:28 +0800

    starting homeRoute requests

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,7 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../models");
 const withAuth = require("../utils/auth");
+
+router.get("/", (req, res) => {
+  "page works";
+});
